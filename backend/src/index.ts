4c03ae751fa00433df3523b80f196f998479b456
MANEPALLI HARI <manepallihari@MANEPALLIs-MacBook-Air.local>
2026-03-10 00:37:23 +0530

    file upload feature added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,29 @@
 import express from "express";
 import cors from "cors";
+import multer from "multer";
 
 const app=express();
 app.use(express.json());
 app.use(cors());
+
+
+const upload=multer({dest:"uploads/"});
+
 app.get("/",(req,res)=>{
 
     res.send("Hello from PDF RAG application!!")
+})
+
+
+app.post("/upload",upload.single("file"),(req,res)=>{
+
+    console.log(req.file);
+
+    res.json({
+        message:"File uploaded successfully",
+        file:req.file
+    })
+
 })
 
 app.listen(5000,()=>{
