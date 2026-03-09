e7b8430b79fc8a6f8764d9e7e0e0536db30ed565
MANEPALLI HARI <manepallihari@MANEPALLIs-MacBook-Air.local>
2026-03-09 18:51:06 +0530

    Initialized both frontend and backend

---
+++ backend/src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+
+const app=express();
+app.use(express.json());
+app.use(cors());
+app.get("/",(req,res)=>{
+
+    res.send("Hello from PDF RAG application!!")
+})
+
+app.listen(5000,()=>{
+    console.log("Server is running on port 5000")
+})
